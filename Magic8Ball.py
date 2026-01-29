31dae5d3f4ad78a0a6f5c07e9e23f9388c94b3e9
ZaneSerhanUNO2029 <12106466@nebraska.edu>
2026-01-29 05:56:35 +0000

    Completed Lab2 Magic 8 Ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -7,18 +7,35 @@
 import random
 
 def main():
-  #Create a list of your responses.
-  answers =[
-    "It is certain.", "It is decidedly so.","Without a doubt.","Yes – definitely.","You may rely on it.",  "As I see it, yes.", "Most likely.",
-    "Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.", "Better not tell you now.","Cannot predict now.",
-    "Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."
-]
+
   print("Magic 8 Ball")
-  #Prompt the user for their question.
-question = input("Ask the Magic 8 Ball a question")
-  #Answer question randomly with one of the options from your earlier list.
+#Create a list of your responses.
+answers = ["It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes, most definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."]
+
+#Prompt the user for their question.
+question = input("Ask the Magic 8 Ball a question    ")
+#Answer question randomly with one of the options from your earlier list.
 response = random.choice(answers)
-print("Response is:", respone)
+print("Response is:", response)
 
 if __name__ == '__main__':
   main()
